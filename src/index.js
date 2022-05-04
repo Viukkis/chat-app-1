ba4a39acae98b2d03bd13c03291c1c4df5b06ef2
Viukkis <viola.ruotsala@tuni.fi>
2022-05-04 20:28:25 +0300

    Week 4: index.js edits, app.css syling, and more.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Login from './components/Login';
+import Register from './components/Register';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
-ReactDOM.render(
+class AppRouter extends React.Component{
+  render() {
+    return (
+      <Router>
+        <div className="app">
+          <nav className="main-nav">
+            <Link to="/">Home</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </nav>
+          <Switch>
+            <Route exact path="/" component={App} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById('root');
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
